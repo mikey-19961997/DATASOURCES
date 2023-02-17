b1d48f0c979fd77fb7bcf4257edbddae25a6f04e
mikey1 <mikey1997@gmail.com>
2023-02-17 11:38:36 +0530

    Added resources

--- main.tf
+++ main.tf
@@ -4,12 +4,17 @@ data "aws_vpc" "myvpc" {
         values = ["192.168.0.0/16"]
     }
 
+data "aws_subnet" "subnets" {
     filter {
-        name = "is-default"
-        values = ["no"]
+        name = "availabilityzone"
+        values = ["us-east-1a","us-east-1b"]
     }
 }
 
 output "vpc_id" {
     value = data.aws_vpc.myvpc.id
+}
+
+output "subnetids" {
+    value = data.aws_subnet.subnets.ids
 }
